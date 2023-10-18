c8305b4a12e4f8d8f9cd317d86542655368ca393
Taylor Murdock <tjmurd4545@gmail.com>
2023-10-18 10:41:43 -0400

    css again

--- src/components/Gear/GearList.tsx
+++ src/components/Gear/GearList.tsx
@@ -29,7 +29,7 @@ function GearList(props: GearListProps) {
 
   return (
     <div className="fixed bottom-8 right-8 flex flex-col items-end justify-center">
-      <div className="mx-auto p-4 bg-white border-green-500 border-4 rounded-lg h-[300px] w-[600px] overflow-y-auto">
+      <div className="mx-auto p-4 bg-white border-green-500 border-4 rounded-lg h-[280px] w-[600px] overflow-y-auto">
         <div className="flex my-8">
           <div style={{ flex: 1, fontWeight: "bold", maxWidth: "50%" }}>
             <p className="text-center">Item</p>
